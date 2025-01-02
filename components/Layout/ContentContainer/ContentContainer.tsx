429bc3b61fe537db5f6edc78b6a366dfcc585d98
Chak Wang Choi <z5413670@ad.unsw.edu.au>
2025-01-02 23:29:43 +0800

    fix: Add noWidthLimit option to Content Container

--- components/Layout/ContentContainer/ContentContainer.tsx
+++ components/Layout/ContentContainer/ContentContainer.tsx
@@ -4,18 +4,21 @@ type ContentContainerProps = {
   /**
    * Has to be valid TailWindCSS class colour name
    */
+  noLimit?: boolean;
   customBackgroundColour?: string;
   children: React.ReactNode;
 };
 
-const ContentContainer = ({
+const ContentContainer = ( {
   children,
+  noLimit = true,
   customBackgroundColour = undefined,
-}: ContentContainerProps): JSX.Element => {
+}: ContentContainerProps ): JSX.Element =>
+{
   return (
-    <section className={`${styles.mainContainer} ${customBackgroundColour ?? ""}`}>
-      <div className={styles.limitingContainer}>
-        <div className={styles.paddingContainer}>{children}</div>
+    <section className={ `${ styles.mainContainer } ${ customBackgroundColour ?? "" }` }>
+      <div className={ `w-full ${ noLimit ?? styles.limitingContainer }` }>
+        <div className={ styles.paddingContainer }>{ children }</div>
       </div>
     </section>
   );
