329d3119bdc0ee330ae246c77bd6269e0642c902
Chak Wang Choi <z5413670@ad.unsw.edu.au>
2025-01-02 23:37:12 +0800

    feat: Add join us section if subcom data is empty on team page

--- components/Profile/SubcomProfileCards.tsx
+++ components/Profile/SubcomProfileCards.tsx
@@ -1,17 +1,27 @@
 import { SubcomProfileData } from "data/teamData";
 import styles from "./SubcomProfileCards.module.scss";
+import Link from "next/link";
 
 type SubcomProfileCardsType = {
+  selectedYear: number;
   subcomData: SubcomProfileData[];
 };
 
-const SubcomProfileCards = ( { subcomData }: SubcomProfileCardsType ): JSX.Element =>
+const SubcomProfileCards = ( { selectedYear, subcomData }: SubcomProfileCardsType ): JSX.Element =>
 {
   return (
     <div className={ styles.subcomMainContainer }>
-      { subcomData.map( ( team, indx ) =>
-      {
-        return (
+      { subcomData.length === 0 ? (
+        <div className={ styles.joinUsMessage }>
+          <Link legacyBehavior href="">
+            <a className="pb-5 px-5">
+              <button className={ styles.buttonStyle }>Join Our Team</button>
+            </a>
+          </Link>
+          <p>Be part of our { selectedYear } team and contribute to something great!</p>
+        </div>
+      ) : (
+        subcomData.map( ( team, indx ) => (
           <div
             className={ styles.subcomContainer }
             key={ `${ team.portfolio } subcommittee section` }
@@ -23,10 +33,11 @@ const SubcomProfileCards = ( { subcomData }: SubcomProfileCardsType ): JSX.Eleme
               ) ) }
             </div>
           </div>
-        );
-      } ) }
+        ) )
+      ) }
     </div>
   );
 };
+
 
 export default SubcomProfileCards;
